33582ff664c892cccd4384446cfea78bf76b24f6
Konstantin Dolinka <Konstantin.kr.ua@gmail.com>
2021-11-11 22:27:48 +0200

    fix register

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function isPalindrom (str) {
     return str == str.split('').reverse().join('');
 }
+
 function findPalindrom (str, separator) {
     const words = str.split(separator);
     let  count=0;
